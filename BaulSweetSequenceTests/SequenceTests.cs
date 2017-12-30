07c9bc26021207871d2cd5c83281af7d0ee1dcb3
William Getz <willegetz@gmail.com>
2017-12-30 11:19:29 -0800

    Can check if a binary value has an odd sequence of 0's

--- BaulSweetSequenceTests/SequenceTests.cs
+++ BaulSweetSequenceTests/SequenceTests.cs
@@ -6,8 +6,8 @@ namespace BaulSweetSequenceTests
     [TestClass]
     public class SequenceTests
     {
-        // Convert number to binary
-        // Drop preceeding 0's
+        // Convert number to binary :: Binary output is a string
+        // Drop preceeding 0's :: Happens on conversion to binary
         //  Minimum number of digits is 1
         // Find all sequences of 0's
         // Does any sequence contain an odd number of 0's?
@@ -17,7 +17,7 @@ namespace BaulSweetSequenceTests
         // Iteration of numbers begins at number 0 and loops until reaching the final number
 
         [TestMethod]
-        public void TestMethod1()
+        public void BinaryConversionTest()
         {
             var startingValue = 1;
 
@@ -25,6 +25,26 @@ namespace BaulSweetSequenceTests
 
             var expected = "1";
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Number2HasOdd0Sequence()
+        {
+            var startingValue = 2;
+            var binaryConversion = Convert.ToString(startingValue, 2);
+
+            var sequencesOf0 = binaryConversion.Split(new Char[] { '1' }, StringSplitOptions.RemoveEmptyEntries);
+            var hasOdd0Sequence = false;
+
+            foreach (var seq in sequencesOf0)
+            {
+                if (seq.Length % 2 == 1)
+                {
+                    hasOdd0Sequence = true;
+                }
+            }
+
+            Assert.IsTrue(hasOdd0Sequence, "The binary value does not contain any sequence of 0 with an odd count");
         }
     }
 }
