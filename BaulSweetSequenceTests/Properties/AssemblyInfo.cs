b600b8e6bcc47016af7886028f2f42e9a20b645f
William Getz <willegetz@gmail.com>
2017-12-30 10:11:14 -0800

    Initial Commit

---
+++ BaulSweetSequenceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BaulSweetSequenceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BaulSweetSequenceTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b4b82b6-fa51-45f1-8d9e-1eb48a4ea09b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BaulSweetSequenceTests/SequenceTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaulSweetSequenceTests
+{
+    [TestClass]
+    public class SequenceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("New creation, nothing tested yet.");
+        }
+    }
+}
