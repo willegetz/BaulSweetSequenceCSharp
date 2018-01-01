cb67405551ca9d6b1ca0d6a3801bb191231be76f
William Getz <willegetz@gmail.com>
2018-01-01 11:52:31 -0800

    Refactoring common functions

--- BaumSweetSequenceTests/SequenceTests.cs
+++ BaumSweetSequenceTests/SequenceTests.cs
@@ -35,24 +35,10 @@ namespace BaumSweetSequenceTests
         public void Number2HasOdd0Sequence()
         {
             var startingValue = 2;
-            var binaryConversion = Convert.ToString(startingValue, 2);
 
-            var sequencesOf0 = binaryConversion.Split(new char[] { '1' }, StringSplitOptions.RemoveEmptyEntries);
-            var hasOdd0Sequence = false;
-            var baumSweetEvaluation = 1;
-
-            foreach (var seq in sequencesOf0)
-            {
-                if (seq.Length % 2 == 1)
-                {
-                    hasOdd0Sequence = true;
-                }
-            }
-
-            if (hasOdd0Sequence)
-            {
-                baumSweetEvaluation = 0;
-            }
+            var binaryConversion = GetBinaryValue(startingValue);
+            var sequencesOf0 = GetBlocksOf0s(binaryConversion);
+            var baumSweetEvaluation = GetBaumSweetEvaluation(sequencesOf0);
 
             var expected = 0;
 
@@ -63,11 +49,56 @@ namespace BaumSweetSequenceTests
         public void Number1DoesNotHaveOdd0Sequence()
         {
             var startingValue = 1;
-            var binaryConversion = Convert.ToString(startingValue, 2);
-            var baumSweetEvaluation = 1;
 
-            var sequencesOf0 = binaryConversion.Split(new char[] { '1' }, StringSplitOptions.RemoveEmptyEntries);
+            var binaryConversion = GetBinaryValue(startingValue);
+            var sequencesOf0 = GetBlocksOf0s(binaryConversion);
+            int baumSweetEvaluation = GetBaumSweetEvaluation(sequencesOf0);
+
+            var expected = 1;
+
+            Assert.AreEqual(expected, baumSweetEvaluation, "Evaluation contains 1 or more blocks of consecutive 0s of odd length");
+        }
+
+        [TestMethod]
+        public void BaumSweetSequenceFor2Is1and1and0()
+        {
+            var baumSweetSequence = new List<int>();
+
+            var numberToEvaluate = 2;
+
+            for (int i = 0; i <= numberToEvaluate; i++)
+            {
+                if (i == 0)
+                {
+                    baumSweetSequence.Add(1);
+                    continue;
+                }
+
+
+                var binaryConversion = GetBinaryValue(i);
+                var sequenceOf0s = GetBlocksOf0s(binaryConversion);
+                var sequenceEvaluation = GetBaumSweetEvaluation(sequenceOf0s);
+
+                baumSweetSequence.Add(sequenceEvaluation);
+            }
+
+            Approvals.VerifyAll(baumSweetSequence, "b_");
+        }
+        
+        private string GetBinaryValue(int startingValue)
+        {
+            return Convert.ToString(startingValue, 2);
+        }
+
+        private string[] GetBlocksOf0s(string binaryConversion)
+        {
+            return binaryConversion.Split(new char[] { '1' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private int GetBaumSweetEvaluation(string[] sequencesOf0)
+        {
             var hasOdd0Sequence = false;
+            var baumSweetEvaluation = 1;
 
             foreach (var seq in sequencesOf0)
             {
@@ -82,48 +113,7 @@ namespace BaumSweetSequenceTests
                 baumSweetEvaluation = 0;
             }
 
-            var expected = 1;
-
-            Assert.AreEqual(expected, baumSweetEvaluation, "Evaluation contains 1 or more blocks of consecutive 0s of odd length");
-        }
-
-        [TestMethod]
-        public void BaumSweetSequenceFor2Is1and1and0()
-        {
-            var numberToEvaluate = 2;
-            var baumSweetSequence = new List<int>();
-
-            for (int i = 0; i <= numberToEvaluate; i++)
-            {
-                if (i == 0)
-                {
-                    baumSweetSequence.Add(1);
-                    continue;
-                }
-
-                var binaryConversion = Convert.ToString(i, 2);
-                var sequenceOf0s = binaryConversion.Split(new char[] { '1' }, StringSplitOptions.RemoveEmptyEntries);
-
-                var hasOdd0Sequence = false;
-                var sequenceEvaluation = 1;
-
-                foreach (var seq in sequenceOf0s)
-                {
-                    if (seq.Length % 2 == 1)
-                    {
-                        hasOdd0Sequence = true;
-                    }
-                }
-
-                if (hasOdd0Sequence)
-                {
-                    sequenceEvaluation = 0;
-                }
-
-                baumSweetSequence.Add(sequenceEvaluation);
-            }
-
-            Approvals.VerifyAll(baumSweetSequence, "b_");
+            return baumSweetEvaluation;
         }
     }
 }
